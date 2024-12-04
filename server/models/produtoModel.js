5ce80863a88795eb0637e185c03735c2418d1913
mateusb261 <mateusbastos261@gmail.com>
2024-12-04 12:30:24 -0400

    produto cadastrando

--- server/models/produtoModel.js
+++ server/models/produtoModel.js
@@ -7,31 +7,39 @@ const getProdutos = async () => {
 };
 
 const createProduto = async (produto) => {
+  try {
+    let codigogtin = produto.gtin;
+    let nomeproduto = produto.nomeProduto;
+    let estoqueminimo = produto.estoqueMinimo;
+    let ativo = 1; // Produto ativo por padrão
+    let dtulttransacao = new Date().toISOString().split('T')[0]; // Data atual no formato YYYY-MM-DD
+    let tipo_produto_idtipoproduto;
 
-  let codigogtin = produto.gtin;
-  let nomeproduto = produto.nomeProduto;
-  let estoqueminimo = produto.estoqueMinimo;
-  let tipo_produto_idtipoproduto;
-  let ativo = 1; // Definindo o produto como ativo por padrão
-  let dtulttransacao = new Date().toISOString().split('T')[0]; // Data atual no formato YYYY-MM-DD
+    // Determinar o tipo de produto
+    if (produto.tipoProduto === 'moveis') {
+      tipo_produto_idtipoproduto = 1;
+    } else if (produto.tipoProduto === 'eletronicos') {
+      tipo_produto_idtipoproduto = 2;
+    } else if (produto.tipoProduto === 'alimentacao') {
+      tipo_produto_idtipoproduto = 3;
+    } else {
+      throw new Error(`Tipo de produto inválido: ${produto.tipoProduto}`);
+    }
 
-  if (produto.tipoProduto === 'moveis') {
-    tipo_produto_idtipoproduto = 1;
+    // Inserção no banco de dados
+    const [result] = await db.execute(
+        'INSERT INTO PRODUTO (codigogtin, nomeproduto, estoqueminimo, ativo, dtulttransacao, tipo_produto_idtipoproduto) VALUES (?, ?, ?, ?, ?, ?)',
+        [codigogtin, nomeproduto, estoqueminimo, ativo, dtulttransacao, tipo_produto_idtipoproduto]
+    );
+
+    console.log('Produto cadastrado com sucesso:', result);
+    return result;
+
+  } catch (error) {
+    console.error('Erro ao cadastrar produto:', error.message);
+    console.error('Detalhes do erro:', error);
+    throw error; // Opcional: relançar o erro para tratamento posterior
   }
-
-  else if (produto.tipoProduto === 'eletronicos') {
-    tipo_produto_idtipoproduto = 2;
-  }
-
-  else if (produto.tipoProduto === 'alimentacao') {
-    tipo_produto_idtipoproduto = 3;
-  }
-
-  const [result] = await db.execute(
-    'INSERT INTO PRODUTO (codigogtin, nomeproduto, estoqueminimo, ativo, dtulttransacao, tipo_produto_idtipoproduto) VALUES (?, ?, ?, ?, ?, ?, ?)',
-    [codigogtin, nomeproduto, estoqueminimo, ativo, dtulttransacao, tipo_produto_idtipoproduto]
-  );
-  return result;
 };
 
 
